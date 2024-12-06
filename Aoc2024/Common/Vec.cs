5607b247d6fff615df73c95c1f1dffd16dd6b94c
Rylan Bueckert <rylan.bueckert@broadsign.com>
2024-12-06 15:04:15 -0700

    Day 4

---
+++ Aoc2024/Common/Vec.cs
@@ -0,0 +1,19 @@
+﻿namespace AoC2024.Common;
+
+public record Vec2D<T>(T X, T Y)
+{
+    public static implicit operator Vec2D<T>((T, T) tuple) =>
+        new Vec2D<T>(tuple.Item1, tuple.Item2);
+
+    public static implicit operator (T, T)(Vec2D<T> vec) =>
+        (vec.X, vec.Y);
+};
+
+public record Vec3D<T>(T X, T Y, T Z)
+{
+    public static implicit operator Vec3D<T>((T, T, T) tuple) =>
+        new Vec3D<T>(tuple.Item1, tuple.Item2, tuple.Item3);
+
+    public static implicit operator (T, T, T)(Vec3D<T> vec) =>
+        (vec.X, vec.Y, vec.Z);
+};
---
+++ Aoc2024/Day4.cs
@@ -0,0 +1,51 @@
+﻿using Aoc2024.Common;
+using AoC2024.Common;
+
+namespace Aoc2024;
+
+public class Day4 : IAocDay
+{
+    public void Run(string inputPath)
+    {
+        var input = InputHelper.ReadGrid(inputPath);
+
+        var word = "MAS";
+        var half = (word.Length - 1) / 2;
+
+        var count = 0;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            for (var j = 0; j < input[i].Length; j++)
+            {
+                if (input[i][j] != word[1]) continue;
+
+                if ((Search(input, word, i - half, j - half, (1, 1)) || Search(input, word, i + half, j + half, (-1, -1))) &&
+                    (Search(input, word, i - half, j + half, (1, -1)) || Search(input, word, i + half, j - half, (-1, 1))))
+                {
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+
+    private static bool Search(char[][] input, string word, int x, int y, Vec2D<int> dir)
+    {
+        for (var i = 0; i < word.Length; i++)
+        {
+            var xi = x + i * dir.X;
+            var yi = y + i * dir.Y;
+            if (!InBounds(input, xi, yi) || input[xi][yi] != word[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool InBounds(char[][] input, int x, int y) =>
+        x >= 0 && x < input.Length && y >= 0 && y < input[x].Length;
+}
