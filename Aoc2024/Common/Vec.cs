3733b6a7adf4977ea13310c82fab571102818f52
Rylan Bueckert <rylan.bueckert@broadsign.com>
2024-12-08 19:18:56 -0700

    day 6

--- Aoc2024/Common/Vec.cs
+++ Aoc2024/Common/Vec.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC2024.Common;
+﻿using Aoc2024.Common;
+
+namespace AoC2024.Common;
 
 public record Vec2D<T>(T X, T Y)
 {
@@ -17,3 +19,18 @@ public record Vec3D<T>(T X, T Y, T Z)
     public static implicit operator (T, T, T)(Vec3D<T> vec) =>
         (vec.X, vec.Y, vec.Z);
 };
+
+public static class VecExtensions
+{
+    public static Vec2D<int> Move(this Vec2D<int> curr, Direction dir, int count = 1)
+    {
+        return dir switch
+        {
+            Direction.Up => (curr.X - count, curr.Y),
+            Direction.Down => (curr.X + count, curr.Y),
+            Direction.Left => (curr.X, curr.Y - count),
+            Direction.Right => (curr.X, curr.Y + count),
+            _ => throw new ArgumentOutOfRangeException(nameof(dir), dir, "Provided direction is not valid.")
+        };
+    }
+}
---
+++ Aoc2024/Day6.cs
@@ -0,0 +1,97 @@
+﻿using Aoc2024.Common;
+using AoC2024.Common;
+
+namespace Aoc2024;
+
+public class Day6 : IAocDay
+{
+    public void Run(string inputPath)
+    {
+        var input = InputHelper.ReadGrid(inputPath);
+
+        var startPos = FindStartPos(input, '^');
+
+        var guardPos = startPos;
+        var guardFacing = Direction.Up;
+        var pathOptions = new HashSet<Vec2D<int>>();
+
+        while (true)
+        {
+            var nextPos = guardPos.Move(guardFacing);
+
+            if (nextPos.X < 0 || nextPos.X >= input.Length || nextPos.Y < 0 || nextPos.Y >= input[nextPos.X].Length)
+                break;
+
+            if (input[nextPos.X][nextPos.Y] == '#')
+            {
+                guardFacing = guardFacing.TurnRight();
+                continue;
+            }
+
+            guardPos = nextPos;
+            pathOptions.Add(guardPos);
+        }
+
+        var loopsFound = 0;
+        foreach (var option in pathOptions)
+        {
+            input[option.X][option.Y] = '#';
+
+            guardPos = startPos;
+            guardFacing = Direction.Up;
+
+            if (HasLoop(input, startPos))
+            {
+                loopsFound++;
+            }
+
+            // Reset
+            input[option.X][option.Y] = '.';
+        }
+
+        Console.WriteLine(loopsFound);
+    }
+
+    private static bool HasLoop(char[][] input, Vec2D<int> startPos)
+    {
+        var guardPos = startPos;
+        var guardFacing = Direction.Up;
+        var visited = new HashSet<(Vec2D<int>, Direction)> { (guardPos, guardFacing) };
+
+        while (true)
+        {
+            var nextPos = guardPos.Move(guardFacing);
+
+            if (visited.Contains((nextPos, guardFacing)))
+                return true;
+
+            if (nextPos.X < 0 || nextPos.X >= input.Length || nextPos.Y < 0 || nextPos.Y >= input[nextPos.X].Length)
+                return false;
+
+            if (input[nextPos.X][nextPos.Y] == '#')
+            {
+                guardFacing = guardFacing.TurnRight();
+                continue;
+            }
+
+            guardPos = nextPos;
+            visited.Add((guardPos, guardFacing));
+        }
+    }
+
+    private static Vec2D<int> FindStartPos(char[][] input, char startChar)
+    {
+        for (var x = 0; x < input.Length; x++)
+        {
+            for (var y = 0; y < input[x].Length; y++)
+            {
+                if (input[x][y] == startChar)
+                {
+                    return new Vec2D<int>(x, y);
+                }
+            }
+        }
+
+        throw new Exception($"Could not find start: '{startChar}'");
+    }
+}
